99deccc5573208fe641449a1f7574459e6eee520
PataiSzabolcs7 <koyazza07@gmail.com>
2024-02-27 22:30:00 +0100

    Login/Register complet

--- MysticRealm/Page/login.php
+++ MysticRealm/Page/login.php
@@ -30,10 +30,8 @@ if (filter_input(
 <section class="h-100 gradient-form bg-login-img">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-xl-10 rounded">
-        <div class="card border border-0 rounded text-black">
-          <div class="row g-0">
-            <div class="col-lg-6">
+      <div class="col-lg-5 rounded">
+        <div class="card border border-0 rounded-5 card-background-glasseffect text-white">  
               <div class="card-body p-md-5 mx-md-4">
 
                 <div class="text-center">
@@ -59,12 +57,12 @@ if (filter_input(
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-outline-primary w-50" type="submit" name="belepesiAdatok" value="true">Login</button>
+                    <button class="btn btn-primary w-50" type="submit" name="belepesiAdatok" value="true">Login</button>
                   </div>
 
                   <div class="d-flex align-items-center justify-content-center pb-4">
                     <p class="mb-0 me-2">Don't have an account?</p>
-                    <a href="index.php?loginswitch=register" class="btn btn-outline-primary">Create new</a>
+                    <a href="index.php?loginswitch=register" class="btn btn-primary">Create new</a>
                   </div>
                   <div class="d-flex align-items-center justify-content-center">
                     <a href="#"><i class="fa-brands fa-square-facebook fa-bounce icon-font"></i></a>
@@ -75,15 +73,6 @@ if (filter_input(
                 </form>
 
               </div>
-            </div>
-            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h1>Mystic Realm</h1>
-                <h4 class="mb-4">Embark on an Epic Fantasy Adventure"</h4>
-                <p class="small mb-0">Mystic Realm is a cutting-edge MMORPG that immerses players in a mesmerizing world of magic, mystery, and mayhem. Set in a sprawling fantasy realm filled with ancient legends and mythical creatures, players are tasked with the monumental quest of saving the land from an encroaching darkness.</p>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
--- MysticRealm/Page/registration.php
+++ MysticRealm/Page/registration.php
@@ -30,17 +30,9 @@ if (filter_input(INPUT_POST, "regisztraciosAdatok", FILTER_VALIDATE_BOOLEAN, FIL
 <section class="h-100 gradient-form bg-login-img" >
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-xl-10">
-        <div class="card rounded-3 border border-0 text-black">
+      <div class="col-lg-5 rounded">
+        <div class="card border border-0 rounded-5 card-background-glasseffect text-white">
           <div class="row g-0">
-          <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h1>Mystic Realm</h1>
-                <h4 class="mb-4">Embark on an Epic Fantasy Adventure"</h4>
-                <p class="small mb-0">Mystic Realm is a cutting-edge MMORPG that immerses players in a mesmerizing world of magic, mystery, and mayhem. Set in a sprawling fantasy realm filled with ancient legends and mythical creatures, players are tasked with the monumental quest of saving the land from an encroaching darkness.</p>
-              </div>
-            </div>
-            <div class="col-lg-6">
               <div class="card-body p-md-5 mx-md-4">
                 
                 <div class="text-center">
@@ -69,18 +61,16 @@ if (filter_input(INPUT_POST, "regisztraciosAdatok", FILTER_VALIDATE_BOOLEAN, FIL
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-outline-primary w-50" type="submit" name="regisztraciosAdatok" value="true">Registration</button>
+                    <button class="btn btn-primary w-50" type="submit" name="regisztraciosAdatok" value="true">Registration</button>
                   </div>
 
                   <div class="d-flex align-items-center justify-content-center pb-4">
                     <p class="mb-0 me-2">Already had account?</p>
-                    <a href="index.php?login_register_switch=logins" class="btn btn-outline-primary">Logins</a>
+                    <a href="index.php?login_register_switch=logins" class="btn btn-primary">Logins</a>
                   </div>
 
                 </form>
-
-              </div>
-            </div>
+             </div>
           </div>
         </div>
       </div>
