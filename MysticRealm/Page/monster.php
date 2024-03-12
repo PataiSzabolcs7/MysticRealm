9801636e362bdeb3cf73833fac47b9bf5c13da7b
PataiSzabolcs7 <koyazza07@gmail.com>
2024-03-12 10:09:41 +0100

    2.6.1

--- MysticRealm/Page/monster.php
+++ MysticRealm/Page/monster.php
@@ -20,3 +20,12 @@
     </div>
   </div>
 </div>
+
+<div class="card" style="width: 18rem;">
+  <img src="./Img/about2.jpg" class=" monster-card-img card-img-top" alt="...">
+  <div class="monster-card-killcount">4213</div>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
