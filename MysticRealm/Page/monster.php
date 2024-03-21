6b2eb000f2ca44a4a9ad54851636553eb20e58ed
PataiSzabolcs7 <koyazza07@gmail.com>
2024-03-21 08:54:52 +0100

    3.4

--- MysticRealm/Page/monster.php
+++ MysticRealm/Page/monster.php
@@ -1,15 +1,25 @@
+
+<!--Call-to-Action Start-->
+<section id="call-to-action2" class="call-to-action2">
+  <div class="container" data-aos="zoom-out">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h1 class="monster-h1">Monster</h1>
+        <p class="monster-p1">"Think twice,before move"</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Call-to-Action End-->
+
 <!--Monster Start-->
 <section class="monster">
-  <div class="container">
-    <div class="section-header">
-      <h2>Monster <hr class="hr1"></h2>
-    </div>
     <div class="container">
       <div class="row">
         <div class="text-center mb-4"><h1>Boss</h1></div>
         <div class="col-lg-4">
-          <div class="card rounded-3 monster_card" style="width: 18rem;">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="...">
+          <div class="card rounded-3 monster_card">
+            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
             <div class="monster-card-killcount">Kill count: 12152</div>
             <div class="card-body">
               <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
@@ -20,8 +30,8 @@
           </div>
         </div>
         <div class="col-lg-4 ">
-          <div class="card rounded-3 monster_card" style="width: 18rem;">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="...">
+          <div class="card rounded-3 monster_card">
+            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
             <div class="monster-card-killcount">Kill count: 12152</div>
             <div class="card-body">
               <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
@@ -32,8 +42,8 @@
           </div>
         </div>
         <div class="col-lg-4">
-          <div class="card rounded-3 monster_card" style="width: 18rem;">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="...">
+          <div class="card rounded-3 monster_card">
+            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
             <div class="monster-card-killcount">Kill count: 12152</div>
             <div class="card-body">
               <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
