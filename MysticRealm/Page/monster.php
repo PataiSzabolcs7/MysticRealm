bd3d69862d80e9f8a1cf46e0b458b1aa1b07f8b3
PataiSzabolcs7 <koyazza07@gmail.com>
2024-03-24 17:56:38 +0100

    3.7

--- MysticRealm/Page/monster.php
+++ MysticRealm/Page/monster.php
@@ -3,57 +3,46 @@
 <section id="call-to-action2" class="call-to-action2">
   <div class="container" data-aos="zoom-out">
     <div class="row justify-content-center">
-      <div class="col-lg-8 text-center">
-        <h1 class="monster-h1">Monster</h1>       
+      <div class="text-center">
+        <h1> Boss </h1>
       </div>
+        <div class="col-lg-4">
+            <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/monster_test.jpg"/>
+                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
+                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
+                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/monster_test.jpg"/>
+                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
+                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
+                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/monster_test.jpg"/>
+                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
+                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
+                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+              </div>
+            </div>
+        </div>
     </div>
   </div>
 </section>
 <!--Call-to-Action End-->
 
-<!--Monster Start-->
-<section class="monster">
-    <div class="container">
-      <div class="row">
-        <div class="text-center mb-4"><h1>Boss</h1></div>
-        <div class="col-lg-4">
-          <div class="card rounded-3 monster_card">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
-            <div class="monster-card-killcount">Kill count: 12152</div>
-            <div class="card-body">
-              <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
-              <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-              <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-              <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 ">
-          <div class="card rounded-3 monster_card">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
-            <div class="monster-card-killcount">Kill count: 12152</div>
-            <div class="card-body">
-              <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
-              <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-              <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-              <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card rounded-3 monster_card">
-            <img src="./Img/monster_test.jpg" class=" monster-card-img card-img-top mx-auto d-block" alt="..." style="width: 75%;">
-            <div class="monster-card-killcount">Kill count: 12152</div>
-            <div class="card-body">
-              <h5 class="card-title text-center monster_name">Monster Name:<br> Red Dragon</h5>
-              <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-              <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-              <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!--Monster end-->
+
+
