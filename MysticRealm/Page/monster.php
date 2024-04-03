c42a9f6805a5ef4f35d79fd0bb03aeebe4175133
PataiSzabolcs7 <koyazza07@gmail.com>
2024-04-03 17:05:48 +0200

    4.0

--- MysticRealm/Page/monster.php
+++ MysticRealm/Page/monster.php
@@ -2,18 +2,56 @@
 <!--Call-to-Action Start-->
 <section id="call-to-action2" class="call-to-action2">
   <div class="container" data-aos="zoom-out">
+    <div class="text-center nft2">
+        <h1> Main-Bosses </h1>
+    </div>
     <div class="row justify-content-center">
-      <div class="text-center">
-        <h1> Boss </h1>
-      </div>
         <div class="col-lg-4">
             <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/Executioner.png"/>
+                <h1 class="monster-h1">Monster name: <br>Executioner</h1>
+                <p class="card-text text-center monster_hp">HP: 1e+07❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 1000⚔️</p>
+                <p class="card-text monster_des">Description: In the dark, unforgiving realm of Erathia, there existed a figure feared by all who dwelled within its shadowed borders - the Executioner.</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
               <div class='mains'>
                 <img class='tokenImage' src="./Img/monster_test.jpg"/>
                 <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
                 <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
                 <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
                 <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/monster_test.jpg"/>
+                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
+                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
+                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+              </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center nft2">
+        <h1> Normal-Mobs </h1>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-4">
+            <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/Executioner.png"/>
+                <h1 class="monster-h1">Monster name: <br>Executioner</h1>
+                <p class="card-text text-center monster_hp">HP: 1e+07❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 1000⚔️</p>
+                <p class="card-text monster_des">Description: In the dark, unforgiving realm of Erathia, there existed a figure feared by all who dwelled within its shadowed borders - the Executioner.</p>
               </div>
             </div>
         </div>
