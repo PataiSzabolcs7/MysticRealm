1daeebb05a59bcdcc63507ec29834225b4404eb7
PataiSzabolcs7 <koyazza07@gmail.com>
2024-04-11 13:52:13 +0200

    4.4

--- MysticRealm/Page/monster.php
+++ MysticRealm/Page/monster.php
@@ -1,5 +1,4 @@
-
-<!--Call-to-Action Start-->
+<!--Main-Boss Start-->
 <section id="call-to-action2" class="call-to-action2">
   <div class="container" data-aos="zoom-out">
     <div class="text-center nft2">
@@ -10,9 +9,11 @@
             <div class="nft">
               <div class='mains'>
                 <img class='tokenImage' src="./Img/Executioner.png"/>
-                <h1 class="monster-h1">Monster name: <br>Executioner</h1>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">Executioner</h1>
                 <p class="card-text text-center monster_hp">HP: 1e+07❤️</p>
                 <p class="card-text text-center monster_atk">ATK: 1000⚔️</p>
+                <p class="card-text text-center monster_atk">Projectile Skill: 500⚔️</p>
                 <p class="card-text monster_des">Description: In the dark, unforgiving realm of Erathia, there existed a figure feared by all who dwelled within its shadowed borders - the Executioner.</p>
               </div>
             </div>
@@ -20,26 +21,76 @@
         <div class="col-lg-4">
         <div class="nft">
               <div class='mains'>
-                <img class='tokenImage' src="./Img/monster_test.jpg"/>
-                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
-                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+                <img class='tokenImage' src="./Img/slimeking.png"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">Slime King</h1>
+                <p class="card-text text-center monster_hp">HP: ???❤️</p>
+                <p class="card-text text-center monster_atk">ATK: ???⚔️</p>
+                <p class="card-text monster_des">Description: In the shadowy depths of the Enchanted Marshlands, where the mist hung heavy and the air buzzed with arcane energy, there reigned a ruler unlike any other: the Slime King.</p>
               </div>
             </div>
         </div>
         <div class="col-lg-4">
         <div class="nft">
               <div class='mains'>
-                <img class='tokenImage' src="./Img/monster_test.jpg"/>
-                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
-                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+                <img class='tokenImage' src="./Img/notfound.jpg"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">???</h1>
+                <p class="card-text text-center monster_hp">HP: ???❤️</p>
+                <p class="card-text text-center monster_atk">ATK: ???⚔️</p>
+                <p class="card-text monster_des">Description: ???</p>
               </div>
             </div>
         </div>
     </div>
+    <!--Main-Boss End-->
+
+    <!--Mini-Boss-Mobs Start-->
+    <div class="text-center nft2">
+        <h1> Mini-Boss-Mobs </h1>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-4">
+            <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/skeletonking.png"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">Skeleton King</h1>
+                <p class="card-text text-center monster_hp">HP: 1000000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 400⚔️</p>
+                <p class="card-text monster_des">Description: In the heart of a desolate wasteland, where the bones of fallen warriors lay scattered like forgotten relics, there ruled a monarch of death and decay: the Skeleton King. </p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/notfound.jpg"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">Brown Slime</h1>
+                <p class="card-text text-center monster_hp">HP: 100000❤️</p>
+                <p class="card-text text-center monster_atk">ATK: 300⚔️</p>
+                <p class="card-text text-center monster_atk">Projectile Skill: 150⚔️</p>
+                <p class="card-text monster_des">Description: In the heart of the mystical Forest of Eldoria, there lived a creature unlike any other: the Big Brown Slime. This colossal slime was the size of a small hill, its gelatinous form pulsating with a warm, earthy hue.</p>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+        <div class="nft">
+              <div class='mains'>
+                <img class='tokenImage' src="./Img/notfound.jpg"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">???</h1>
+                <p class="card-text text-center monster_hp">HP: ???❤️</p>
+                <p class="card-text text-center monster_atk">ATK: ???⚔️</p>
+                <p class="card-text monster_des">Description: ???</p>
+              </div>
+            </div>
+        </div>
+    </div>
+    <!--Mini-Boss-Mobs End-->
+
+    <!--Normal-Mobs Start-->
     <div class="text-center nft2">
         <h1> Normal-Mobs </h1>
     </div>
@@ -71,18 +122,19 @@
         <div class="col-lg-4">
         <div class="nft">
               <div class='mains'>
-                <img class='tokenImage' src="./Img/monster_test.jpg"/>
-                <h1 class="monster-h1">Monster name: <br>Red Dragon</h1>
-                <p class="card-text text-center monster_hp">HP: 42000-120000❤️</p>
-                <p class="card-text text-center monster_atk">ATK: 17000-39000⚔️</p>
-                <p class="card-text monster_des">Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nesciunt hic</p>
+                <img class='tokenImage' src="./Img/notfound.jpg"/>
+                <h1 class="monster-h1">Monster name:</h1>
+                <h1 class="monster-h1 text-center">???</h1>
+                <p class="card-text text-center monster_hp">HP: ???❤️</p>
+                <p class="card-text text-center monster_atk">ATK: ???⚔️</p>
+                <p class="card-text monster_des">Description: ???</p>
               </div>
             </div>
         </div>
     </div>
   </div>
 </section>
-<!--Call-to-Action End-->
+<!--Normal-Mobs End-->
 
 
 
