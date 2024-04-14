ee1ee4f7ee72f19b75a4775c2106b75ba0b936b3
PataiSzabolcs7 <koyazza07@gmail.com>
2024-04-14 12:18:26 +0200

    4.6

---
+++ MysticRealm/Page/settings.php
@@ -0,0 +1,26 @@
+<section class="settings">
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <div class="form-container">
+            <h2>Password Change</h2>
+            <form id="passwordForm">
+            <div class="form-group">
+                <label for="currentPassword">Current Password</label>
+                <input type="password" class="form-control" id="currentPassword" required>
+            </div>
+            <div class="form-group">
+                <label for="newPassword">New Password</label>
+                <input type="password" class="form-control" id="newPassword" required>
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword">Confirm New Password</label>
+                <input type="password" class="form-control" id="confirmPassword" required>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Change Password</button>
+            </form>
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
